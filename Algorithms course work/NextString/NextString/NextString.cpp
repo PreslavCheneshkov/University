90ec8339fa0e758f9cc162d55e41d0b95dc45de4
PreslavCheneshkov <101942358+PreslavCheneshkov@users.noreply.github.com>
2024-10-27 20:44:49 +0200

    added algorithms course work

---
+++ Algorithms course work/NextString/NextString/NextString.cpp
@@ -0,0 +1,121 @@
+﻿#include <iostream>
+#include <set>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+vector<char> getUniqueCharacters(string& inputString)
+{
+	vector<char> uniqueCharactersList;
+	set<char> uniqueCharactersSet;
+	for (int i = 0; i < inputString.size(); i++)
+	{
+		uniqueCharactersSet.insert(inputString[i]);
+	}
+	for (auto& character : uniqueCharactersSet)
+	{
+		uniqueCharactersList.push_back(character);
+	}
+	return uniqueCharactersList;
+}
+
+char getNextCharacter(char currentCharacter, vector<char>& uniqueCharacters)
+{
+	for (int i = 0; i < uniqueCharacters.size(); i++)
+	{
+		if (currentCharacter >= uniqueCharacters[i])
+			continue;
+
+		return uniqueCharacters[i];
+	}
+}
+
+bool isLargestCharacter(char character, vector<char>& uniqueCharacters)
+{
+	bool isLargestCharacter = character == uniqueCharacters.at(uniqueCharacters.size() - 1);
+	return isLargestCharacter;
+}
+
+string getActualInputString(string& inputString, int targetStringLength, char smallestCharacter)
+{
+	int stringSize = inputString.size();
+	if (targetStringLength == stringSize)
+		return inputString;
+	if (targetStringLength < stringSize)
+	{
+		return inputString.substr(0, targetStringLength);
+	}
+	stringstream ss;
+	ss << inputString;
+
+	for (int i = 0; i < targetStringLength - stringSize; i++)
+		ss << smallestCharacter;
+	return ss.str();
+}
+
+void handleInput(int& inputStringLength, string& inputString, int& outputStringLength)
+{
+	cout << "Please enter n = ";
+	cin >> inputStringLength;
+	cout << "Please enter the input string: ";
+	cin >> inputString;
+	cout << "Please enter k = ";
+	cin >> outputStringLength;
+
+	if (inputStringLength < 1 || inputStringLength > 100000 ||
+		outputStringLength < 1 || outputStringLength > 100000 ||
+		inputString.size() != inputStringLength)
+	{
+		cout << "\n\nInvalid input!\nValid input:\n1 <= n <= 100000\n1 <= k <= 100000\nInput string size must be equal to n!\nTry again!\n\n";
+		exit(1);
+	}
+}
+
+int main()
+{
+	int inputStringLength, outputStringLength;
+	string inputString;
+	handleInput(inputStringLength, inputString, outputStringLength);
+
+	vector<char> uniqueCharacters = getUniqueCharacters(inputString);
+	string actualString = getActualInputString(inputString, outputStringLength, uniqueCharacters.at(0));
+	if (uniqueCharacters.size() == 1)
+	{
+		cout << "\n\n=================================================================\n\n";
+		cout << actualString << " is the only possible string!";
+		cout << "\n\n=================================================================\n\n";
+		exit(0);
+	}
+
+	int addedZeroCount = 0;
+	bool concatRemainder = false;
+	stringstream outputStringStream;
+	for (int i = actualString.size() - 1; i >= 0; i--)
+	{
+		if (!concatRemainder)
+		{
+			if (isLargestCharacter(actualString[i], uniqueCharacters))
+			{
+				addedZeroCount++;
+				continue;
+			}
+			while (addedZeroCount > 0)
+			{
+				outputStringStream << uniqueCharacters.at(0);
+				addedZeroCount--;
+			}
+			outputStringStream << getNextCharacter(actualString[i], uniqueCharacters);
+			concatRemainder = true;
+			continue;
+		}
+		outputStringStream << actualString[i];
+	}
+
+	string reversedOutputString = outputStringStream.str();
+	reverse(reversedOutputString.begin(), reversedOutputString.end());
+	cout << "\n\n=================================================================\n\n";
+	cout << "The next string in alphabetical order is: " << reversedOutputString;
+	cout << "\n\n=================================================================\n\n";
+}
